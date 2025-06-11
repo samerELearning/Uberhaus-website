2f7e6afc6238b0b58e01490a1d897cb1a399af09
Samer Saber <samerelearning@outlook.com>
2025-06-11 12:14:48 +0200

    Implement hero CSS

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,5 +1,5 @@
 import react, {use, useeffect, useState} from 'react';
-import "../styles/Hero.css";
+import "./Components/hero.css";
 
 const images = [
   "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
